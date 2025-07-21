b1731825704ec74d2dcafccebdce9a9123636f83
two_spoons <twospoonstv@gmail.com>
2025-07-21 15:21:25 +1000

    feat: Add purgecss.config.js

---
+++ purgecss.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  content: ["./_site/**/*.html"], // Path to your HTML files
+  css: ["./assets/css/*.css"], // Path to your CSS files
+  safelist: [], // Add any classes you want to keep here
+};
